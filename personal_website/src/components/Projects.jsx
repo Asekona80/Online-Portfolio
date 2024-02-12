92122a155541e4976aa4ac0acb9b4dbaa0d53648
Asekhona80 <asekhnax@gmail.com>
2024-02-12 10:30:27 +0200

    populating the images

--- personal_website/src/components/Projects.jsx
+++ personal_website/src/components/Projects.jsx
@@ -19,6 +19,8 @@ const Projects = () => {
     <div  className='w-full bg-[#FBE4FF] py-16 px-4'>
         <div className='max-w-[1240px] mx-auto'>
         <h1 className='text-3xl font-bold mb-8 px-4 text-center'>PROJECT</h1>
+        <slider {...settings}/>
+        <div></div>
         </div>
     </div>
   )

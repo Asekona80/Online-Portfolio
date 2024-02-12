93178c66c16e5e949e5964952ef0939730cf90db
Asekhona80 <asekhnax@gmail.com>
2024-02-12 13:24:23 +0200

    fixing few things on contact

--- personal_website/src/components/Contact.jsx
+++ personal_website/src/components/Contact.jsx
@@ -1,12 +1,58 @@
-import React from 'react'
+import React from 'react';
 
 const Contact = () => {
   return (
     <div className='w-full bg-[#f3f3ec] py-16 px-4'>
-        <h1 className='text-3xl font-bold mb-8 px-4 text-center'>CONTACT</h1>
-
+      <h1 className='text-3xl font-bold mb-8 px-4 text-center'>CONTACT</h1>
+      <div className='max-w-4xl mx-auto flex flex-col lg:flex-row'>
+        <div className='bg-white rounded-lg p-8 mb-8 lg:mr-8 shadow-md border border-gray-300'>
+          <form>
+            <div className='mb-4'>
+              <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='name'>
+                Name
+              </label>
+              <input
+                className='appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                id='name'
+                type='text'
+                placeholder='Enter name'
+              />
+            </div>
+            <div className='mb-4'>
+              <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='email'>
+                Email
+              </label>
+              <input
+                className='appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                id='email'
+                type='email'
+                placeholder='Enter email'
+              />
+            </div>
+            <div className='mb-4'>
+              <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='message'>
+                Message
+              </label>
+              <textarea
+                className='appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                id='message'
+                placeholder='Send message'
+                rows='4'
+              ></textarea>
+            </div>
+            <div className='flex justify-center lg:justify-start'>
+              <button
+                className='bg-[#BCB1FF] text-black font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline shadow-md'
+                type='button'
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;

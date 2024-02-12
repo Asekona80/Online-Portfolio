2f89014b501a6fc8c19ceae0eb5e1c1649d6192b
Asekhona80 <asekhnax@gmail.com>
2024-02-12 11:33:53 +0200

    contact component

---
+++ personal_website/src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='w-full bg-[#FBE4FF] py-16 px-4'>
+
+    </div>
+  )
+}
+
+export default Contact
--- personal_website/src/components/Projects.jsx
+++ personal_website/src/components/Projects.jsx
@@ -1,26 +1,18 @@
 import React from 'react'
 import Slider from 'react-slick'
-
-
-
+import Project1 from '../assets/Project1.jpg'
+import Project3 from '../assets/Project3.jpg'
+import Project4 from '../assets/Project4.jpg'
 
 const Projects = () => {
-    const settings={
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-
-    }
+  
   return (
     <div  className='w-full bg-[#FBE4FF] py-16 px-4'>
-        <div className='max-w-[1240px] mx-auto'>
+        <div className='flex items-center justify-center flex-col h-screen   max-w-[1240px] mx-auto'>
         <h1 className='text-3xl font-bold mb-8 px-4 text-center'>PROJECT</h1>
-        <slider {...settings}/>
-        <div></div>
+   
+        <div>
+        </div>
         </div>
     </div>
   )

9bf9a32d0704e4f981a4750b5f2bcf9790e50aee
Asekhona80 <asekhnax@gmail.com>
2024-02-12 11:36:53 +0200

    populating some content for contact

--- personal_website/src/components/Contact.jsx
+++ personal_website/src/components/Contact.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='w-full bg-[#FBE4FF] py-16 px-4'>
+    <div className='w-full bg-[#f3f3ec] py-16 px-4'>
+        <h1 className='text-3xl font-bold mb-8 px-4 text-center'>CONTACT</h1>
 
     </div>
   )
--- personal_website/src/components/Projects.jsx
+++ personal_website/src/components/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
   
   return (
     <div  className='w-full bg-[#FBE4FF] py-16 px-4'>
-        <div className='flex items-center justify-center flex-col h-screen   max-w-[1240px] mx-auto'>
+        <div className='max-w-[1240px] mx-auto'>
         <h1 className='text-3xl font-bold mb-8 px-4 text-center'>PROJECT</h1>
    
         <div>

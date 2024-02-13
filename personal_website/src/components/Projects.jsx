853c554764470b9be26d4d451822db9fe5a2ea1d
Asekhona80 <asekhnax@gmail.com>
2024-02-13 09:52:37 +0200

    swiper images

--- personal_website/src/components/Projects.jsx
+++ personal_website/src/components/Projects.jsx
@@ -2,21 +2,24 @@ import React from 'react';
 import {swiper, swiperslide} from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/pagination'
-import 'swiper/css/freee-mode'
-import {Freemode, pagination} from 'swiper/modules'
+import 'swiper/css/free-mode'
+import {FreeMode, Pagination} from 'swiper/modules'
+import {RxArrowTopRight} from 'react-icons/rx'
 
 
 
 const Projects = () => {
 
-  };
-
-  return (
-    <div className='w-full bg-[#FBE4FF] py-16 px-4'>
-      <h1 className='text-3xl font-bold mb-8 px-4 text-center'>PROJECT</h1>
+  return (    
+      <div className='flex items-center justify-center flex-col h-screen bg-[#FBE4FF]'>
+          <h1 className='text-3xl font-bold mb-8 px-4 text-center'>PROJECT</h1>
   
-    </div>
-  );
+      </div>
+
+  
+  )
+  }
+  
 
 
 export default Projects;

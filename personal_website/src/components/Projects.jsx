2695d5c5ea0470ea40b90e7f2aefb216dc47d5a3
Asekhona80 <asekhnax@gmail.com>
2024-02-13 10:14:49 +0200

    image slider

--- personal_website/src/components/Projects.jsx
+++ personal_website/src/components/Projects.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import {swiper, swiperslide} from 'swiper/react'
-import 'swiper/css'
-import 'swiper/css/pagination'
-import 'swiper/css/free-mode'
-import {FreeMode, Pagination} from 'swiper/modules'
-import {RxArrowTopRight} from 'react-icons/rx'
-
-
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Image1 from '../assets/Image1.jpg';
+import Image2 from '../assets/Image2.jpg';
+import Image3 from '../assets/Image3.jpg';
 
 const Projects = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
 
-  return (    
-      <div className='flex items-center justify-center flex-col h-screen bg-[#FBE4FF]'>
-          <h1 className='text-3xl font-bold mb-8 px-4 text-center'>PROJECT</h1>
-  
-      </div>
-
-  
-  )
-  }
-  
-
+  return (
+    <div className='w-full bg-[#FBE4FF] py-16 px-4'>
+      <h1 className='text-3xl font-bold mb-8 px-4 text-center'>PROJECT</h1>
+      <Slider {...settings}>
+        <div>
+          <img src={Image1} alt="Image1" className="mx-auto max-w-full" />
+        </div>
+        <div>
+          <img src={Image2} alt="Image2" className="mx-auto max-w-full" />
+        </div>
+        <div>
+          <img src={Image3} alt="Image3" className="mx-auto max-w-full" />
+        </div>
+      </Slider>
+    </div>
+  );
+};
 
 export default Projects;

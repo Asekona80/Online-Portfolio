19b930aea5b9269ea1be65e4ac023db2c1c46803
Asekhona80 <asekhnax@gmail.com>
2024-02-13 09:26:37 +0200

    importing swiper

--- personal_website/src/components/Projects.jsx
+++ personal_website/src/components/Projects.jsx
@@ -1,35 +1,22 @@
 import React from 'react';
-import Slider from 'react-slick';
-import { FaGithub } from 'react-icons/fa';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Project1 from '../assets/Project1.jpg';
-import Project3 from '../assets/Project3.jpg';
+import {swiper, swiperslide} from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/freee-mode'
+import {Freemode, pagination} from 'swiper/modules'
+
+
 
 const Projects = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
+
   };
 
   return (
     <div className='w-full bg-[#FBE4FF] py-16 px-4'>
       <h1 className='text-3xl font-bold mb-8 px-4 text-center'>PROJECT</h1>
-      <Slider {...settings} className="flex justify-center">
-        <div className="relative">
-          <img src={Project1} alt="Project 1" className="mx-auto" />
-          <FaGithub className="absolute bottom-0 left-0 mb-2 ml-2 text-white text-2xl cursor-pointer" />
-        </div>
-        <div className="relative">
-          <img src={Project3} alt="Project 3" className="mx-auto" />
-          <FaGithub className="absolute bottom-0 right-20 mb-2 ml-2 text-white text-2xl cursor-pointer" />
-        </div>
-      </Slider>
+  
     </div>
   );
-};
+
 
 export default Projects;

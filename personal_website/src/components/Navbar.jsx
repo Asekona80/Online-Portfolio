80a7e695d39b4cbe167bdc4b2fff3b3a63727995
Asekhona80 <asekhnax@gmail.com>
2024-02-20 15:00:12 +0200

    size og the toggle bar fixed

--- personal_website/src/components/Navbar.jsx
+++ personal_website/src/components/Navbar.jsx
@@ -40,29 +40,29 @@ const Navbar = () => {
   };
 
   return (
-    <div className={`md:flex justify-between items-center mx-auto px-4 text-black font-semibold transition-all duration-500 ease-in-out ${isSticky ? 'sticky top-0 bg-gradient-to-b from-indigo-200 to-[#f0fae5] shadow-md z-50 rounded-b-full' : 'absolute top-0 w-full'}`}>
+    <div className={`md:flex justify-between items-center mx-auto px-4 py-4 text-black font-semibold transition-all duration-500 ease-in-out ${isSticky ? 'sticky top-0 bg-gradient-to-b from-indigo-200 to-[#f0fae5] shadow-md z-50 rounded-b-full' : 'absolute top-0 w-full'}`}>
       {!nav && <div className="hidden md:block"> {/* Conditionally render logo on desktop */}
-        <img src={logo} alt='Logo' className='w-[95px] h-10' />
+        <img src={logo} alt='Logo' className='w-[95px] h-10 ml-4' />
       </div>}
       <div className="md:hidden flex justify-end mr-4"> {/* Adjust padding and margin */}
-        <AiOutlineMenu onClick={handleNav} className="text-xl cursor-pointer p-2" /> {/* Adjust the size and add padding */}
+        <AiOutlineMenu onClick={handleNav} className="text-4xl cursor-pointer p-2" /> 
       </div>
       <div className={`md:hidden ${nav ? 'block fixed inset-0 bg-[#f0fae5] round-full shadow-lg z-50' : 'hidden'}`}> {/* Conditionally render mobile menu items based on 'nav' state */}
         <div className='flex flex-col justify-center items-center'>
           <img src={logo} alt="Logo" className="w-24 mr-8" />
-          <button onClick={() => scrollToSection('Home')} className='w-[75%] flex justify-center rounded-full shadow-lg bg-gray-100 shadow-indigo-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 hover:text-gray-900'>
+          <button onClick={() => scrollToSection('home')} className='w-[75%] flex justify-center rounded-full shadow-lg bg-gray-100 shadow-indigo-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 hover:text-gray-900'>
             <AiOutlineHome size={20} />
             <span className='pl-4'>Home</span>
           </button>
-          <button onClick={() => scrollToSection('About')} className='w-[75%] flex justify-center rounded-full shadow-lg bg-gray-100 shadow-indigo-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 hover:text-gray-900'>
+          <button onClick={() => scrollToSection('about')} className='w-[75%] flex justify-center rounded-full shadow-lg bg-gray-100 shadow-indigo-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 hover:text-gray-900'>
             <BsPerson size={20} />
             <span className='pl-4'>About Me</span>
           </button>
-          <button onClick={() => scrollToSection('Projects')} className='w-[75%] flex justify-center rounded-full shadow-lg bg-gray-100 shadow-indigo-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 hover:text-gray-900'>
+          <button onClick={() => scrollToSection('projects')} className='w-[75%] flex justify-center rounded-full shadow-lg bg-gray-100 shadow-indigo-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 hover:text-gray-900'>
             <AiOutlineProject size={20} />
             <span className='pl-4'>Projects</span>
           </button>
-          <button onClick={() => scrollToSection('Contact')} className='w-[75%] flex justify-center rounded-full shadow-lg bg-gray-100 shadow-indigo-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 hover:text-gray-900'>
+          <button onClick={() => scrollToSection('contact')} className='w-[75%] flex justify-center rounded-full shadow-lg bg-gray-100 shadow-indigo-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 hover:text-gray-900'>
             <AiOutlineMail size={20} />
             <span className='pl-4'>Contact</span>
           </button>
